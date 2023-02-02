3164ff8ce22ebaae6944aeb8cf6c0307de46b155
Biel Serrano Sanchez <bielsesa@gmail.com>
2023-02-02 23:23:09 +0100

    added new info to about

--- routes/about.tsx
+++ routes/about.tsx
@@ -4,23 +4,66 @@ import NavMenu from "../islands/NavMenu.tsx";
 import ContactForm from "../islands/ContactForm.tsx";
 
 export const handler: Handlers = {
-    async GET(req, ctx) {
-        const resp = await ctx.render();
-        resp.headers.set("X-Custom-Header", "Hello");
-        return resp;
-    }
-}
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
 
 // this is ctx.render
 export default function AboutPage() {
-    return (
-        <>
-            <Header />
-            <NavMenu />
-            <div class="p-4 mx-auto max-w-screen-md">
-                <p>Yada yada</p>
-                <ContactForm />
-            </div>
-        </>  
-    );
+  return (
+    <>
+      <Header />
+      <NavMenu />
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="py-5 font-semibold">
+          Hi! My name is Biel, and I am currently working as a software
+          developer in Barcelona, Spain.
+        </p>
+        <p class="py-3">
+          I am non-binary, and use both
+          <span class="text-indigo-500 px-1">they/them</span>
+          and<span class="text-indigo-500 px-1">he/him</span>
+          pronouns (masculine pronouns mainly in Spanish).
+        </p>
+        <p class="py-2">
+          My true wish is to work as a game developer. Alongside my job, I am
+          enrolled in an Associate Degree where we study the various tasks a
+          game developer must do in their day to day work.
+        </p>
+        <p class="py-2">
+          I am attending
+          <a
+            class="transition ease-in-out duration-500 hover:underline hover:text-indigo-500 px-1"
+            href="https://www.cevbarcelona.com/formacion-a-distancia/cfgs-en-animaciones-3d-juegos-y-entornos-interactivos-a-distancia/"
+          >
+            CEV - Centre de Comucació, Imatge i So.
+          </a>
+        </p>
+        <p class="py-3">
+          I speak Spanish and Catalan natively, have an upper-intermediate level
+          in English, and a beginner level in Japanese. I love learning new
+          languages, I think it is really interesting how differently languages
+          can work.
+        </p>
+        <p class="font-semibold pt-5">
+          If you could support me, it would be wonderful! Thank you!
+        </p>
+        <div class="py-2">
+          <script
+            type="text/javascript"
+            src="https://storage.ko-fi.com/cdn/widget/Widget_2.js"
+          >
+          </script>
+          <script type="text/javascript">
+            kofiwidget2.init('Support Me on Ko-fi', '#663931',
+            'Q5Q76WDM');kofiwidget2.draw();
+          </script>
+        </div>
+        {/* <ContactForm /> */}
+      </div>
+    </>
+  );
 }
--- routes/portfolio.tsx
+++ routes/portfolio.tsx
@@ -2,18 +2,26 @@ import Header from "../islands/Header.tsx";
 import NavMenu from "../islands/NavMenu.tsx";
 
 export default function PortfolioPage() {
-    return (
-        <>
-            <Header />
-            <NavMenu />
-            <div class="p-4 mx-auto max-w-screen-md">
-                <p class="pb-2">Here you can see some of my personal projects</p>
-                <div class="flex p-4 mx-auto max-w-screen-md">
-                    <iframe src="https://itch.io/embed/669926?bg_color=222222&amp;fg_color=eeeeee&amp;border_color=363636" width="208" height="167" frameBorder="0">
-                        <a href="https://astronautpotato.itch.io/medical-showdown">Medical Showdown by astronautpotato</a>
-                    </iframe>
-                </div>
-            </div>
-        </>  
-    );
+  return (
+    <>
+      <Header />
+      <NavMenu />
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="pb-2">Here you can see some of my personal projects</p>
+        <div class="flex p-4 mx-auto max-w-screen-md">
+          <iframe
+            class="rounded-lg"
+            src="https://itch.io/embed/669926?bg_color=222222&amp;fg_color=eeeeee&amp;border_color=363636"
+            width="208"
+            height="167"
+            frameBorder="0"
+          >
+            <a href="https://astronautpotato.itch.io/medical-showdown">
+              Medical Showdown by astronautpotato
+            </a>
+          </iframe>
+        </div>
+      </div>
+    </>
+  );
 }
