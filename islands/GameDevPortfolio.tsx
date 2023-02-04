58f82d76025925674f21604d1bf0819554b0f966
Biel Serrano Sanchez <bielsesa@gmail.com>
2023-02-04 12:39:10 +0100

    base for interactive & separated portfolio

---
+++ islands/GameDevPortfolio.tsx
@@ -0,0 +1,24 @@
+export default function GameDevPortfolio() {
+    return (
+        <div class="grid grid-flow-row p-1 mx-auto max-w-screen-md">
+            <iframe
+                class="rounded-lg m-2 mx-auto w-52 sm:w-full"
+                src="https://itch.io/embed/669926?link_color=663931"
+                frameBorder="0"
+            >
+                <a href="https://astronautpotato.itch.io/medical-showdown">
+                    Medical Showdown by astronautpotato
+                </a>
+            </iframe>
+            <iframe
+                class="rounded-lg m-2 mx-auto w-52 sm:w-full"
+                src="https://itch.io/embed/1905682?link_color=663931"
+                frameBorder="0"
+            >
+                <a href="https://astronautpotato.itch.io/shooter-2d-demo">
+                    Shooter 2D demo by astronautpotato
+                </a>
+            </iframe>
+        </div>
+    );
+}
