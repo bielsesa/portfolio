d870753c73bd0600b9c52003ce18992652377f40
Biel Serrano Sanchez <bielsesa@gmail.com>
2023-02-08 13:51:16 +0100

    test, add custom color extension to twind

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,11 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#D6B0B0'
+      }
+    }
+  }
 } as Options;
